290ee504ac44d0018a88b2e0c320ee64a1aac4a4
Jeff Williams <jwill1258@gmail.com>
2020-03-29 23:12:50 -0400

    updated db

--- managers/schema.sql
+++ managers/schema.sql
@@ -15,13 +15,13 @@ CREATE TABLE products(
 SELECT * FROM products;
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Uncharted 4", "Video Games", 49.95, 150),
-  ("DOOM", "Video Games", 59.99, 200),
-  ("Crate of Spam", "Food and Drink", 24.50, 50),
-  ("Cool Shades", "Apparel", 75.00, 5),
-  ("Worn Denim Jeans", "Apparel", 54.25, 35),
-  ("Survival Towel", "Necessities", 42.42, 42),
-  ("Bill and Ted's Excellent Adventure", "Films", 15.00, 25),
-  ("Mad Max: Fury Road", "Films", 25.50, 57),
-  ("Monopoly", "Board Games", 30.50, 35),
-  ("Yahtzee", "Board Games", 19.95, 23);
+VALUES ("Gameboy", "Electronics", 89.95, 150),
+  ("NBA2K20", "Electronics", 59.99, 200),
+  ("White Tee", "Clothing", 14.50, 5),
+  ("Black Tee", "Clothing", 16.25, 35),
+  ("Shampoo", "Necessities", 11.50, 42),
+  ("Avatar", "Electronics", 25.00, 25),
+  ("Lion King", "Electronics", 25.50, 57),
+  ("Checkers", "Games", 15.50, 35),
+  ("Chess", "Games", 19.99, 23);
+)
